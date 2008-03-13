683fce44418b44b17117040e8102b78dedccf1de
Alex Payne <al3x@al3x.net>
2008-03-12 22:09:49 -0700

    ignore logs, and a ready to rock site

--- whereisbritt.rb
+++ whereisbritt.rb
@@ -16,10 +16,12 @@ get '/' do
           font-family: Helvetica, Arial, "sans-serif";
           font-size: 62.5%;
           margin: 10px 30% 0 30%;
+          text-align: center;
           width: auto;
         }
-        h1 { font-size: 6em; }
-        p { font-size: 4em; }
+        h1 { font-size: 7em; }
+        p { font-size: 3.5em; }
+        .info_text { font-size: 2em;}
         #container {
          clear: both;
          margin: auto;
@@ -31,7 +33,29 @@ get '/' do
         function load() {
           if (GBrowserIsCompatible()) {
             var map = new GMap2(document.getElementById("map"));
-            map.setCenter(new GLatLng(37.4419, -122.1419), 13);
+            map.setCenter(new GLatLng(51.4788, -0.0107), 1);
+            
+            var brittIcon = new GIcon();
+            brittIcon.image = "http://static.al3x.net/britt_icon.png";
+            brittIcon.shadow = "http://labs.google.com/ridefinder/images/mm_20_shadow.png";
+            brittIcon.iconSize = new GSize(30, 30);
+            brittIcon.shadowSize = new GSize(46, 31);
+            brittIcon.iconAnchor = new GPoint(6, 20);
+            brittIcon.infoWindowAnchor = new GPoint(5, 1);
+            
+            var bounds = map.getBounds();
+            var southWest = bounds.getSouthWest();
+            var northEast = bounds.getNorthEast();
+            var lngSpan = northEast.lng() - southWest.lng();
+            var latSpan = northEast.lat() - southWest.lat();
+            var point = new GLatLng(southWest.lat() + latSpan * Math.random(), southWest.lng() + lngSpan * Math.random());
+            var marker = new GMarker(point, { title: "Britt", icon: brittIcon });
+            
+            GEvent.addListener(marker, "click", function() {
+              marker.openInfoWindowHtml('<span class="info_text">Britt</span>');
+            });
+            
+            map.addOverlay(marker);
           }
         }
         //]]>
@@ -40,7 +64,6 @@ get '/' do
     <body onload="load()" onunload="GUnload()">
       <div id="container">
         <h1>Where is Britt?</h1>
-        <p>Britt is in <span class="location"><%= "random location" %></span></p>
         <div id="map" style="width: 500px; height: 300px"></div>
         <p>That's where Britt is!</p>
       </div>
